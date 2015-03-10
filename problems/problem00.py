f6994a1f602fceac89c480fc4d05682caa62a00b
Franklin Chen <franklinchen@franklinchen.com>
2015-03-10 16:46:22 -0400

    Change spec of problem 0.

--- problems/problem00.py
+++ problems/problem00.py
@@ -2,10 +2,10 @@ def add_fuzz_factor(i):
     """Return i with an additional mystery fuzz factor.
 
     >>> add_fuzz_factor(5)
-    7
+    6
 
     >>> add_fuzz_factor(-1)
-    1
+    0
     """
 
     return i
