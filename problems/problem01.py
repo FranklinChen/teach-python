63c508c50ca791a8730b2b33c5a1d0832c2558e9
Franklin Chen <franklinchen@franklinchen.com>
2015-03-10 16:56:28 -0400

    Add problem 1, for conditional control flow.

---
+++ problems/problem01.py
@@ -0,0 +1,17 @@
+def print_multiples_of_numbers(i, j, scale):
+    """For integers i through j (including j itself), if i <= j,
+    print each integer multiplied by scale. If i > j, print
+    'the range is invalid' instead.
+
+    Please use a for loop, although while is possible here also.
+
+    Remember to write doctests to cover all cases.
+
+    >>> print_multiples_of_numbers(20, 19, 1)
+    the range is invalid
+
+    >>> print_multiples_of_numbers(3, 5, 1)
+    3
+    4
+    5
+    """
