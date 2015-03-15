bdafe9a0134bac500eae844635a53c98a7004dd3
Franklin Chen <franklinchen@franklinchen.com>
2015-03-15 11:13:50 -0400

    Problem 03 question.

---
+++ problems/problem03.py
@@ -0,0 +1,14 @@
+def get_multiples_of_numbers(i, j, scale):
+    """Exact same problem as problem 02, but please rewrite your code to
+    use a list comprehension:
+
+    https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
+
+    Copy your tests from problem 02 here. Later we will cover
+    how to not duplicate tests for different implementations of the
+    same function.
+
+    >>> copy your tests to here.
+    """
+
+    return 'do it'
