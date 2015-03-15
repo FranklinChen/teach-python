0351540b6114dced2d67c324b9ebbb5ea711ae17
Franklin Chen <franklinchen@franklinchen.com>
2015-03-15 11:24:34 -0400

    Problem 04 question.

---
+++ problems/problem04.py
@@ -0,0 +1,16 @@
+def get_sum_to_6(start, end):
+    """Return a list of all pairs (i, j) where both i and j are
+    >= start and <= end, i <= j, and i+j is 6.
+
+    Use a list comprehension:
+
+    https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
+
+    >>> get_sum_to_6(1, 5)
+    [(1, 5), (2, 4), (3, 3)]
+
+    >>> get_sum_to_6(10, 12)
+    []
+    """
+
+    return 'junk'
