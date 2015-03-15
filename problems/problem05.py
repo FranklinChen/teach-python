eccfd69125214389cec4e7b2e8c4735db66583b5
Franklin Chen <franklinchen@franklinchen.com>
2015-03-15 11:36:27 -0400

    Problem 05 question.

---
+++ problems/problem05.py
@@ -0,0 +1,22 @@
+def choose_action(action, i, j):
+    """If action is 'multiples' then delegate to problem 3's
+    function with i and j with scale of 3,
+    else if action is 'sum', delegate to
+    problem 4's function with the start and end,
+    else return 'unknown action'
+
+    Remember to use "from...import..."
+
+    https://docs.python.org/2/tutorial/modules.html
+
+    >>> choose_action('junk', 2, 5)
+    'unknown action'
+
+    >>> choose_action('sum', -1, 7)
+    [(-1, 7), (0, 6), (1, 5), (2, 4), (3, 3)]
+
+    >>> choose_action('multiples', 10, 17)
+    [30, 33, 36, 39, 42, 45, 48, 51]
+    """
+
+    return 'nonsense'
