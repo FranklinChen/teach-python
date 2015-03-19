882c69b55fdfa677f0d730916bcffe6ef30ff3c4
Franklin Chen <franklinchen@franklinchen.com>
2015-03-19 15:18:36 -0400

    Problem 07a: egg worker.

---
+++ problems/problem07a.py
@@ -0,0 +1,45 @@
+"""Functions are values just like integers and strings.
+"""
+
+
+def scramble_worker(egg):
+    """Scramble an egg."""
+    return 'scrambled ' + egg
+
+
+def boil_worker(egg):
+    """Boil an egg."""
+    return 'boiled ' + egg
+
+
+def test_scramble():
+    f = hire_egg_worker('scramble')
+    assert f('brown egg') == 'scrambled brown egg'
+    assert f('white egg') == 'scrambled white egg'
+
+
+def test_boil():
+    assert hire_egg_worker('boil')('funny egg') == 'boiled funny egg'
+
+
+def test_huh():
+    f = hire_egg_worker('huh?')
+    assert f('brown egg') == 'help me with brown egg!'
+    assert f('green egg') == 'help me with green egg!'
+
+
+def hire_egg_worker(which):
+    """If which is 'scramble', then return a worker that scrambles an egg when given an egg; if which is 'boil', then return a worker that boils an egg when given an egg; otherwise, return a worker that is confused about what to do with eggs and returns an appropriate error message asking for help on the egg.
+    """
+
+    if which == 'scramble':
+        return scramble_worker
+    elif which == 'boil':
+        return boil_worker
+    else:
+        return confused_worker
+
+
+def confused_worker(egg):
+    """Confused about the egg."""
+    return 'help me with ' + egg + '!'
