6d7a5b6682f583d412b9a769e3043343acfb3e15
Franklin Chen <franklinchen@franklinchen.com>
2015-03-19 12:45:54 -0400

    Switch to py.test.

--- problems/problem09.py
+++ problems/problem09.py
@@ -1,63 +1,34 @@
-"""Lists can contain other lists, just as in real life a box can
-contain other boxes. A very important concept in computing is the
-tree. A Tree[A] of values of some type A is a dictionary with two
-keys:
+from problem06 import trivial_tree, small_tree, big_tree
 
-  - root: a root of type A
-  - kids: a list of Tree[A] (called the children)
 
-Note that a tree is defined recursively.
-"""
-
-"""
-An example of Tree[int] that is just a root with no children.
-"""
-trivial_tree = {
-    'root': 42,
+trivial_tree_doubled = {
+    'root': 84,
     'kids': []
 }
 
-"""
-An example of Tree[str] that is only one level deep.
-"""
-small_tree = {
-    'root': 'Anna',
-    'kids': [
-        {'root': 'Vasili', 'kids': []},
-        {'root': 'Sophia', 'kids': []},
-        {'root': 'Ari', 'kids': []},
-    ]
-}
-
-"""
-A Tree[int] that involves not only children, but also grandchildren
-and great-grandchildren.
-
-You may want to draw this out.
-"""
-big_tree = {
-    'root': 1,
+big_tree_doubled = {
+    'root': 2,
     'kids': [
         {
-            'root': 2, 'kids': [
+            'root': 4, 'kids': [
                 {
-                    'root': 3,
+                    'root': 6,
                     'kids': [
                         {
-                            'root': 4,
+                            'root': 8,
                             'kids': []
                         }
                     ]
                 },
                 {
-                    'root': 5,
+                    'root': 10,
                     'kids': []
                 },
                 {
-                    'root': 6,
+                    'root': 12,
                     'kids': [
                         {
-                            'root': 7,
+                            'root': 14,
                             'kids': []
                         }
                     ]
@@ -65,16 +36,16 @@ big_tree = {
             ]
         },
         {
-            'root': 8, 'kids': [
+            'root': 16, 'kids': [
                 {
-                    'root': 9,
+                    'root': 18,
                     'kids': []
                 },
                 {
-                    'root': 10,
+                    'root': 20,
                     'kids': [
                         {
-                            'root': 11,
+                            'root': 22,
                             'kids': []
                         }
                     ]
@@ -85,29 +56,50 @@ big_tree = {
 }
 
 
+def test_double_trivial():
+    assert tree_double(trivial_tree) == trivial_tree_doubled
+
+
+def test_double_big():
+    assert tree_double(big_tree) == big_tree_doubled
+
+
 def tree_double(tree):
     """Return a new tree[int] of the same shape but with every integer
     value doubled.
-
-    >>> tree_double(trivial_tree)
-    {'kids': [], 'root': 84}
-
-    >>> tree_double(big_tree)
-    {'kids': [{'kids': [{'kids': [{'kids': [], 'root': 8}], 'root': 6}, {'kids': [], 'root': 10}, {'kids': [{'kids': [], 'root': 14}], 'root': 12}], 'root': 4}, {'kids': [{'kids': [], 'root': 18}, {'kids': [{'kids': [], 'root': 22}], 'root': 20}], 'root': 16}], 'root': 2}
     """
 
-    return 'junk'
+    return {
+        'root': 'junk root'
+        'kids': 'junk kids'
+        ]
+    }
+
+
+small_tree_greeted = {
+    'root': 'hello Anna',
+    'kids': [
+        {'root': 'hello Vasili', 'kids': []},
+        {'root': 'hello Sophia', 'kids': []},
+        {'root': 'hello Ari', 'kids': []},
+    ]
+}
+
+
+def test_greet_small():
+    assert tree_greet(small_tree) == small_tree_greeted
 
 
 def tree_greet(tree):
     """Return a new tree[str] of the same shape but with a greeting
     prepended.
-
-    >>> tree_greet(small_tree)
-    {'kids': [{'kids': [], 'root': 'hello Vasili'}, {'kids': [], 'root': 'hello Sophia'}, {'kids': [], 'root': 'hello Ari'}], 'root': 'hello Anna'}
     """
 
-    return 'junk'
+    return {
+        'root': 'junk root'
+        'kids': 'junk kids'
+        ]
+    }
 
 
 """
@@ -137,21 +129,50 @@ medium_tree = {
 }
 
 
+medium_tree_doubled = {
+    'root': 6,
+    'kids': [
+        {
+            'root': 2,
+            'kids': [
+                {
+                    'root': 8,
+                    'kids': []
+                },
+                {
+                    'root': 2,
+                    'kids': []
+                }
+            ]
+        },
+        {
+            'root': 10,
+            'kids': []
+        }
+    ]
+}
+
+
+def test_map_double_medium():
+    def doubler(n):
+        return n * 2
+    tree_doubler = tree_map(doubler)
+    assert tree_doubler(medium_tree) == medium_tree_doubled
+
+
+def test_map_greet_small():
+    def greeter(s):
+        return 'hello ' + s
+    tree_greeter = tree_map(greeter)
+    assert tree_greeter(small_tree) == small_tree_greeted
+
+
 def tree_map(f):
     """Return a function from tree to tree that applies the function f
     to each value in the tree.
-
-    >>> def doubler(n):
-    ...     return n * 2
-    >>> tree_doubler = tree_map(doubler)
-    >>> tree_doubler(medium_tree)
-    {'kids': [{'kids': [{'kids': [], 'root': 8}, {'kids': [], 'root': 2}], 'root': 2}, {'kids': [], 'root': 10}], 'root': 6}
-
-    >>> def greeter(s):
-    ...     return 'hello ' + s
-    >>> tree_greeter = tree_map(greeter)
-    >>> tree_greeter(small_tree)
-    {'kids': [{'kids': [], 'root': 'hello Vasili'}, {'kids': [], 'root': 'hello Sophia'}, {'kids': [], 'root': 'hello Ari'}], 'root': 'hello Anna'}
     """
 
-    return 'junk'
+    def result(tree):
+        return 'junk'
+
+    return result
