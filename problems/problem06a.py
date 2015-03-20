322188b7f54c16f63b9c67132721c422ee159ff5
Franklin Chen <franklinchen@franklinchen.com>
2015-03-20 12:56:28 -0400

    Question: Problem 06a: review lists, dictionaries.

---
+++ problems/problem06a.py
@@ -0,0 +1,15 @@
+"""Review lists and dictionaries."""
+
+
+def number_of_items_in_list(the_list):
+    """Return the number of items in the_list"""
+
+    return -1
+
+
+def number_of_rooms_in_house(house):
+    """house is a dictionary that has a key 'rooms' where the value is
+    a list of rooms. Return the number of rooms in the house.
+    """
+
+    return -1
