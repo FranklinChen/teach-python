5976e11f120e0235769f60831a5b4095299e4f07
Franklin Chen <franklinchen@franklinchen.com>
2015-03-20 12:23:54 -0400

    Factor out pytests from problem 07c.

--- problems/problem07c.py
+++ problems/problem07c.py
@@ -1,24 +1,5 @@
-def formal_greet(name):
-    return 'hello ' + name
-
-
-def informal_greet(name):
-    return 'hi ' + name
-
-
-def test_greetings():
-    greeting_line = [formal_greet,
-                     informal_greet,
-                     informal_greet,
-                     formal_greet]
-    assert all_greetings(greeting_line, "John") == ['hello John',
-                                                    'hi John',
-                                                    'hi John',
-                                                    'hello John']
-
-
 def all_greetings(greeting_committee, name):
-    """Given a list of greeters, return a list of all the greetings performed
+    """Given a list of greeters who return a greeting when given a name, return a list of all the greetings performed.
     for name.
     """
 
