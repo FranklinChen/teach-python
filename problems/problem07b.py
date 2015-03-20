83802fb9e575a8e92f5ab23d849017b9fd83e54a
Franklin Chen <franklinchen@franklinchen.com>
2015-03-20 12:34:36 -0400

    Question: Factor out pytests from problem 07b.

--- problems/problem07b.py
+++ problems/problem07b.py
@@ -2,29 +2,8 @@
 """
 
 
-def formal_greet(name):
-    return 'hello ' + name
-
-
-def informal_greet(name):
-    return 'hi ' + name
-
-
-def test_formal():
-    f = hire_greeter('stiff')
-    assert f('Joseph') == 'hello Joseph'
-
-
-def test_informal():
-    assert hire_greeter('loose')('Mary') == 'hi Mary'
-
-
-def test_huh():
-    f = hire_greeter('huh?')
-    assert f('Bob') == 'Guten Morgen Bob'
-
-
 def hire_greeter(which):
-    """If which is 'stiff', return a formal greeter, else if 'loose', an informal greeter, else a German greeter.
+    """If which is 'stiff', return a formal greeter, else if 'loose',
+    an informal greeter, else a German greeter.
     """
     return 'junk'
